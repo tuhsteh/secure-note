764225b6fb01796d2b4b3b1321a69dd6aed57cb0
tommy tom <t.stear@gmail.com>
2017-12-17 21:53:18 -0700

    initial commit

---
+++ requirements.md
@@ -0,0 +1,6 @@
+#  Prerequisites:
+
+1.  Install Spring-boot-cli
+    ```$ brew tap pivotal/tap
+    $ brew install springboot```
+1.  
---
+++ src/main/java/com/tommy/securenote/SecureNoteApplication.java
@@ -0,0 +1,12 @@
+package com.tommy.securenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureNoteApplication.class, args);
+	}
+}
