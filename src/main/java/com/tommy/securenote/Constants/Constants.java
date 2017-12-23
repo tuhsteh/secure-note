d929769cadf8a39d72a6c14eb66c1f8220feb19b
john <john@kuru.local>
2017-12-22 17:26:39 -0700

    outlining a tiny bit

---
+++ src/main/java/com/tommy/securenote/Constants/Constants.java
@@ -0,0 +1,13 @@
+package com.tommy.securenote.Constants;
+
+public class Constants {
+
+    private Constants() {
+        // restrict instansiation
+    }
+
+    // URL MAPPINGS
+    public static final String INDEX = "/";
+    public static final String FETCH_NOTE = "/fetch";
+    public static final String CREATE_NOTE = "/create";
+}
