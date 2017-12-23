6779740c6273f04b07a5c6041048c1e72dc6d040
SpaceWhale <john.sosoka@protonmail.ch>
2017-12-23 10:56:13 -0700

    more blind outlining

---
+++ src/main/java/com/tommy/securenote/controller/JsonController.java
@@ -0,0 +1,9 @@
+package com.tommy.securenote.controller;
+
+/**
+ *  Splitting out controllers. I think we should have HTML/JSON controllers with mirrored request mappings
+ *  so that a browser or application could consume the app.
+ */
+
+public class JsonController {
+}
